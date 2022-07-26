9490825a953bd0a8b11bb9693a7da96d44e938ea
session-kniP <sessionknip@gmail.com>
2022-07-26 14:15:47 +0300

    [PSQUARE-4] Rework sequence validation API // Validation fixes

--- src/main/java/com/purrer/gentools/validation/SequenceValidationImpl.java
+++ src/main/java/com/purrer/gentools/validation/SequenceValidationImpl.java
@@ -29,9 +29,6 @@ public class SequenceValidationImpl implements SequenceValidation {
             return false;
         }
 
-        if (sequence.length() % 2 != 0) {
-            return false;
-        }
         for (int j = 0; j < sequence.length() - 1; j += 2) {
             if (!String.valueOf(sequence.charAt(j)).equalsIgnoreCase(String.valueOf(sequence.charAt(j + 1)))) {
                 return false;
